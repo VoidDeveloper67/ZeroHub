01685a10474d8748928acfe09ac6fa72a2a00f8c
VoidDeveloper67 <juniortype123@gmail.com>
2026-05-16 06:24:57 +0100

    Add GameList.lua with game URLs

---
+++ GameList.lua
@@ -0,0 +1,14 @@
+local Games = {
+  [8202280624] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/Bite%20By%20Night", -- Bite By Night
+  [9649298941] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/survive%20lava%20for%20brainrots.lua", -- Survive Lava for Brainrots
+  [7709344486] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/SAB%20Duels", -- SAB Duels
+  [9186719164] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/Sailor%20Piece", -- Sailor Piece
+  [994732206]  = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/VoidHub%20PvP", -- Blox Fruits
+  [9787206684] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/Be%20A%20Lucky%20Block", -- Be a Lucky Block
+  [9098570654] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/STA.luau", -- Survive The Apocalypse
+  [10004244222] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/Kick%20A%20Lucky%20Block", -- Kick a Lucky Block
+  [9792947201] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/Rng-Slime.luau", -- Slime RNG
+  [9348272796] = "https://raw.githubusercontent.com/VoidDeveloper67/VoidHub-Scripts/refs/heads/main/Survive%20Zombie%20Arena.luau", -- Survive Zombie Arena
+}
+
+return Games

b6553f560a18c4f087990eba79dbd80b8d9a0c94
VoidDeveloper67 <juniortype123@gmail.com>
2026-05-16 06:31:29 +0100

    Update GameList URL for ZeroHub

--- ZeroHub.lua
+++ ZeroHub.lua
@@ -1,4 +1,4 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/VoidDeveloper67/Void-Hub/refs/heads/main/GameList.lua"))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/VoidDeveloper67/ZeroHub/refs/heads/main/GameList.lua"))()
 
 local URL = Games[game.GameId]
 

fc2b34d50709575f1119bdd75abbf9bfcb6ca73c
VoidDeveloper67 <juniortype123@gmail.com>
2026-05-26 22:56:32 +0100

    Add Blox Fruits to Games list

--- GameList.lua
+++ GameList.lua
@@ -1,5 +1,6 @@
 local Games = {
   [9186719164] = "https://raw.githubusercontent.com/VoidDeveloper67/ZeroHub-Script/refs/heads/main/Sailor%20Piece", -- Sailor Piece
+  [994732206] = "https://raw.githubusercontent.com/VoidDeveloper67/ZeroHub-Script/refs/heads/main/Blox%20Fruits", -- Blox Fruits
 }
 
 return Games
